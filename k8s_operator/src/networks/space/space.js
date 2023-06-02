94e7bcf952fed2b0ea2da29df772335459b12f6e
liujun10 <liujun10@qianxin.com>
2023-06-02 19:54:00 +0800

    创建删除修改模板空间

--- k8s_operator/src/networks/space/space.js
+++ k8s_operator/src/networks/space/space.js
@@ -15,3 +15,39 @@ export function createSpaceTemplate(form) {
         }
     })
 }
+//获取模板类型
+export function getTemplateKind() {
+    return requests({
+        url:'/template/kind/get',
+        method:'get',
+    })
+}
+//获取空间模板
+export function getSpaceTemplate() {
+    return requests({
+        url:'/space/template/get',
+        method:'get',
+    })
+}
+//编辑空间模板
+export function editSpaceTemplate(form,id) {
+    return requests({
+        url:'/space/template/update/' + id,
+        method:'put',
+        data:{
+            "name": form.name,
+            "kind_id": form.kind,
+            "image": form.image,
+            "tags": form.tags,
+            "description": form.description,
+            "avatar": form.avatar,
+        }
+    })
+}
+//删除空间模板
+export function deleteSpaceTemplate(id) {
+    return requests({
+        url:'/space/template/delete/' + id,
+        method:'delete'
+    })
+}
